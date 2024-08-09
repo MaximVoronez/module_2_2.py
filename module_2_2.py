7191cad9262e9288fb6ff9d3caab7bc150d8b921
MaximVoronez <maxsemerikov2012@mail.ru>
2024-08-10 00:55:07 +0300

    module_2_2.py
    Операторы if, elif, else."

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+#first, second и third
+print('введите данные first:')
+first= ('веденное сисло =',int(input()))
+print('введите данные second:')
+second =('веденное сисло =', int(input()))
+print('введите данные third')
+third=('веденное сисло =', int(input()))
+print('выполняется программа')
+if first==second and third: # и
+    print(' ответ 2',)
+elif first==second  or third : # или
+    print('ответ 3')
+else:
+    print('ответ 0', ) # иначе
+
+#elif first==second or third:
+    #print('три числа равны, ответ 2')
+#else: print('ноль числа неравны, ответ 0',)
+
